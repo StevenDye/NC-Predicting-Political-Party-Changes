33fcd400e182bddcc1e205657101ac75de7f4a18
irvingcampbell <irving.campbell@gmail.com>
2019-12-03 14:08:42 -0500

    updates to files before creating slide show

--- data_prep.py
+++ data_prep.py
@@ -11,7 +11,7 @@ df.change_dt = pd.to_datetime(df.change_dt)
 # Dropping rows with Libertarian, Green, or Constitutional party
 
 def party_drop(party, dataframe):
-	"""This function removes unwanted political parties"""
+    """This function removes unwanted political parties"""
     rows = dataframe.loc[(dataframe.party_from == party) | (dataframe.party_to == party)]
     dataframe = dataframe.drop(rows.index)
     return dataframe
