ceecc8598ec00793e25ac484c7295c6a4d89deb5
Steven Dye <steven.p.dye@gmail.com>
2019-12-03 13:12:43 -0500

    Uploading data_prep.py and nc_functions.py

--- data_prep.py
+++ data_prep.py
@@ -1,7 +1,11 @@
+"""This file cleans data/2019_party_change_list.cs and outouts X_train.csv,
+X_test.csv, y_train.csv, and y_test.csv"""
+
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
+from nc_functions import party_drop
 
 RANDOM_STATE = 5627
 
@@ -9,18 +13,9 @@ df = pd.read_csv('data/2019_party_change_list.csv').drop_duplicates()
 df.change_dt = pd.to_datetime(df.change_dt)
 
 # Dropping rows with Libertarian, Green, or Constitutional party
-
-def party_drop(party, dataframe):
-	"""This function removes unwanted political parties"""
-    rows = dataframe.loc[(dataframe.party_from == party) | (dataframe.party_to == party)]
-    dataframe = dataframe.drop(rows.index)
-    return dataframe
-
-
 dropped_parties = ['LIB', 'GRE', 'CST']
-for party in dropped_parties:
-    df = party_drop(party, df)
-
+for pol_party in dropped_parties:
+	df = party_drop(pol_party, df)
 
 # Hot encode
 X, y = df[['county_id', 'party_from']], df.party_to
@@ -37,9 +32,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=RANDOM_ST
 smt = SMOTE()
 X_train, y_train = smt.fit_sample(X_train, y_train)
 
-# Cross-Validate
+# Convert to dataframes
+X_train_df = pd.DataFrame(X_train)
+y_train_df = pd.DataFrame(y_train)
+X_test_df = pd.DataFrame(X_test)
+y_test_df = pd.DataFrame(y_test)
 
 # Print cleaned data
-# cleaned_training_data = df.copy()
-# cleaned_test_data = 
-# pi_df.to_csv('df_cleaned.csv', sep=',')
+X_train_df.to_csv('data/X_train.csv', sep=',')
+y_train_df.to_csv('data/y_train.csv', sep=',')
+X_test_df.to_csv('data/X_test.csv', sep=',')
+y_test_df.to_csv('data/y_test.csv', sep=',')
